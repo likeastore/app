199cc19471d7d89ae69f5aaf6bf7692d09419e8d
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-03-30 22:18:35 +0200

    couchdb start2

---
+++ src/db/db.js
@@ -0,0 +1,22 @@
+var config = require('../../local_settings.js');
+var cradle = require('cradle');
+
+var connection = new cradle.Connection(
+	config.couchdb.url, 
+	config.couchdb.port, { 
+		auth: { 
+			username: config.couchdb.admin, 
+			password: config.couchdb.pass 
+		}
+});
+
+function getDb (dbname) {
+	if (!dbname || typeof dbname !== 'string') {
+		console.error('Incorrect database name');
+	}
+	return connection.database(dbname);
+}
+
+module.exports = {
+	getDb: getDb
+};
