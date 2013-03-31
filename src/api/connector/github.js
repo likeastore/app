fdbb12729e2643519e63e153c021ddaf2db3e2d5
alexander.beletsky <alexander.beletsky@gmail.com>
2013-03-31 09:28:56 +0300

    removed unused api methods

--- src/api/connector/github.js
+++ src/api/connector/github.js
@@ -1,13 +1,6 @@
 var repository = require('./../../db/stars.js');
 
 function connector(app) {
-	app.get('/api/connector/github', function (req, res) {
-		repository.all(function (err, docs) {
-			// TODO: if error return 500
-			res.end(docs);
-		});
-	});
-
 	app.post('/api/connector/github', function (req, res) {
 		var stars = req.body.stars;
 
