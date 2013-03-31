1800a769bb4e7fe6fec5eb0c53ddf3f7d2a7f932
alexander.beletsky <alexander.beletsky@gmail.com>
2013-03-31 07:35:04 +0300

    get items endpoints added

--- src/api/connector/twitter.js
+++ src/api/connector/twitter.js
@@ -1,6 +1,13 @@
 var repository = require('./../../db/favorites.js');
 
 function connector(app) {
+	app.get('/api/connector/github', function (req, res) {
+		repository.all(function (err, docs) {
+			// TODO: if error return 500
+			res.end(docs);
+		});
+	});
+
 	app.post('/api/connector/twitter', function (req, res) {
 		var favorites = req.body;
 
