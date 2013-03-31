f4977148f8551e899499c8a4ec5c933a77b11908
alexander.beletsky <alexander.beletsky@gmail.com>
2013-03-31 13:27:43 +0300

    shake

--- src/connectors/twitter.js
+++ src/connectors/twitter.js
@@ -8,16 +8,8 @@ module.exports = {
 
 		console.log('Handshaking twitter on:' + twitterConnectorUrl);
 
-		repository.removeAll(serviceHandshake);
-
-		function serviceHandshake(err) {
-			if (err) {
-				return callback(err);
-			}
-
-			request.post(twitterConnectorUrl, {form: {postUrl: postUrl}}, function(err, response) {
-				return callback(err, response);
-			});
-		}
+		request.post(twitterConnectorUrl, {form: {postUrl: postUrl}}, function(err, response) {
+			return callback(err, response);
+		});
 	}
 };
