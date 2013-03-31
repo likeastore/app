ba59282cef64a192255d1d82ad2392d310db7f47
alexander.beletsky <alexander.beletsky@gmail.com>
2013-03-31 09:38:47 +0300

    items endpoint implemented

--- src/api/items.js
+++ src/api/items.js
@@ -3,15 +3,32 @@ var stars = require('./../db/stars.js');
 
 function items(app) {
 	app.get('/api/items', function (req, res) {
+		favorites.all(function (err, favs) {
+			if (err) {
+				return res.send(500);
+			}
 
+			stars.all(function (err, strs) {
+				if (err) {
+					return res.send(500);
+				}
+
+				var all = favs.concat(strs);
+				return res.json(all);
+			});
+		});
 	});
 
 	app.get('/api/items/github', function (req, res) {
-
+		stars.all(function (err, strs) {
+			return res.json(strs);
+		});
 	});
 
 	app.get('/api/items/twitter', function (req, res) {
-
+		favorites.all(function (err, favs) {
+			return res.json(favs);
+		});
 	});
 }
 
