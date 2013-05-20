fb6d048729f538d5e54bb370c18c3df03ed042ae
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-05-20 20:59:34 +0300

    linkify add-on

--- public/js/directives/linkify.js
+++ public/js/directives/linkify.js
@@ -1,20 +1,18 @@
 define(function (require) {
 	'use strict';
 
-	var angular = require('angular');
-
 	function Linkify () {
 		return {
 			restrict: 'A',
 			link: function (scope, elem, attr) {
 				var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-				var desc = scope.$eval(attr.linkify);
+				var text = scope.$eval(attr.linkify);
 
-				var result = desc.replace(urlRegex, function (url) {
+				var result = text.replace(urlRegex, function (url) {
 					return '<a href="' + url + '" target="_blank">' + url + '</a>';
 				});
 
-				angular.element(elem).html(result);
+				elem.html(result);
 			}
 		};
 	}
