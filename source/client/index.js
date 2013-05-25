1b907bb7586582046df3c496d93a4ca9b1b4f413
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-05-25 16:14:39 +0300

    ie fixes

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main-3309d07622398a200ee670fedd877166.js',
+	js: '/build/main-ef7339dfaa9b0a899d72171592ba620a.js',
 	css: '/build/main-7de87be443bde1bcb16b0bd6971d515e.css'
 };
