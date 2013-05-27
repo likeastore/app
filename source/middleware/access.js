12742cd56891fa4dfde352e28397e5ace7b3c550
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-05-27 17:24:41 +0300

    auth logged

--- source/middleware/access.js
+++ source/middleware/access.js
@@ -5,15 +5,21 @@ var config = require('likeastore-config');
 
 function authenticated () {
 	return function (req, res, next) {
+		logger.info({message: 'authentication check'});
+
 		if (req.isAuthenticated() || req.role === 'guest') {
+			logger.info({message: 'user is ' + (req.role === 'guest' ? 'guest' : 'authenticated')});
 			return next();
 		}
+
+		logger.info({message: 'user is not authenticated and will be redirected', url: req.url, body: req.body });
 		res.redirect('/welcome');
 	};
 }
 
 function guest () {
 	return function (req, res, next) {
+		logger.info({message: 'ensuring guest access for request', request: req});
 		req.role === 'guest';
 		return next();
 	};
@@ -21,6 +27,8 @@ function guest () {
 
 function invite () {
 	return function (req, res, next) {
+		logger.info({message: 'checking invintation permissions for request', request: req});
+
 		var inviteId = req.cookies.likeastoreInviteId || req.cookies.likeastore_invite_id;
 
 		if (!inviteId || typeof inviteId !== 'string') {
@@ -41,12 +49,15 @@ function invite () {
 
 function ensureUser () {
 	return function (req, res, next) {
+		logger.info({message: 'ensuring we have user in session', request: req, user: req.user });
+
 		var urls = ['/api', '/connect'];
 		var acceptUrl = _.any(urls, function (url) {
 			return req.url.substr(0, url.length) === url;
 		});
 
 		if (acceptUrl && !req.user) {
+			logger.warning({message: 'unauthorized access (sending 401)', url: req.url, request: req});
 			return res.send(401);
 		}
 
@@ -60,6 +71,8 @@ function redirectUnauthorized() {
 		res.end = function (data, encoding) {
 			res.end = end;
 			if (res.statusCode === 401) {
+				logger.info({message: 'redirect unauthorized', request: req, user: req.user });
+
 				return res.redirect(config.siteUrl);
 			}
 
