61998bb6cce3f0a90b2f334cbc6b70be38ae8b6b
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-06-25 23:03:05 +0300

    prepared testing framework

--- tools/cleanDb.js
+++ tools/cleanDb.js
@@ -1,11 +1,12 @@
 var db = require('./../source/db/dbConnector').db;
 
-console.log('cleaning up likestore db...')
+console.log('cleaning up likestore db...');
+
 db.dropDatabase();
 db.close(function (err) {
 	if (err) {
 		return console.log('clean up failed: ' + err);
 	}
 
-	return console.log('likestore db is clean now.');
+	console.log('likestore db is clean now.');
 });
