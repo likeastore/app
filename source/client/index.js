f7ce507ae111390b8324328228e9af9946e9aa8c
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-07-27 16:36:40 +0300

    build

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
 	js: '/build/main-da8ae88f788b55d24d0eafa70b680a0a.js',
-	css: '/build/main-c8170196775995ce38108dae1f9411b5.css'
+	css: '/build/main-d019e0c0e8b0c38658797db7537461b3.css'
 };
