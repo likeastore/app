caa6aa6149d348ebf676ede6f2211c2348872991
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-07-27 18:10:31 +0300

    build

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
 	js: '/build/main-da8ae88f788b55d24d0eafa70b680a0a.js',
-	css: '/build/main-0ab52daac4be32d1b3a4f71574c4895f.css'
+	css: '/build/main-15abcd7ee5cf8c84e059fd7da0e0029d.css'
 };
