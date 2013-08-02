a6269848f91401eb9cfe70d97ae798e8d22e6e23
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-08-02 16:03:15 +0300

    added warning to user

--- source/api/users.js
+++ source/api/users.js
@@ -1,10 +1,15 @@
+var _ = require('underscore');
 var users = require('../db/users.js');
 var items = require('../db/items.js');
-var nets = require('../db/networks.js');
+var networks = require('../db/networks.js');
 var logger = require('../utils/logger');
 
 function usersService (app) {
-	app.get('/api/users/me', getUser);
+	app.get('/api/users/me',
+		getUser,
+		disabledNetworksWarning,
+		returnUser
+	);
 
 	function getUser(req, res, next) {
 		users.findByEmail(req.user, function (err, user) {
@@ -12,8 +17,28 @@ function usersService (app) {
 				return next(err);
 			}
 
-			res.json(user);
+			req.readUser = user;
+
+			next();
 		});
+	}
+
+	function disabledNetworksWarning(req, res, next) {
+		networks.findNetworks(req.user, function (err, networks) {
+			if (err) {
+				return next(err);
+			}
+
+			req.readUser.warning = _.any(networks, function (network) {
+				return network.disabled;
+			});
+
+			next();
+		});
+	}
+
+	function returnUser(req, res, next) {
+		res.json(req.readUser);
 	}
 }
 
