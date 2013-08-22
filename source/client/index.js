14f826c8f32a530717aa52982fbe76fadb37deee
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-08-22 22:04:04 +0300

    small css add-on && build

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
 	js: '/build/main-5cc3f7dc7757c01bf68fba9cdf7dff85.js',
-	css: '/build/main-cc101d41f13fb83cfa15ddb7f781c85b.css'
+	css: '/build/main-e672c8c475fc343c8d74683560844af2.css'
 };
