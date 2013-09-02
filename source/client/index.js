8d5aea90d7b8a3c4dc84623d8237b76a314fdbab
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-09-02 11:17:02 +0200

    built

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main-3f64cef3c84aacb16613b755f1f6ced0.js',
+	js: '/build/main-c6a4037749e12a044fa6566d92eb124c.js',
 	css: '/build/main-ad3bfe64b946f86637712dd688ca2d93.css'
 };
