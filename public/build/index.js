925373590f1f9316903ecde2b0fead49b1c0594c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-09-21 16:41:28 +0300

    build

--- public/build/index.js
+++ public/build/index.js
@@ -1,4 +1,4 @@
 module.exports = {
 	js: '/build/main-980262e812d41d561a720bb82df6daff.js',
-	css: '/build/main-7677cbcac8725fdadbe5c9048618f7bc.css'
+	css: '/build/main-de9fdf2c5a5ed0c3f465c2bed018fee8.css'
 };
