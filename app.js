7a3028944ff2f630b5f79811f8e7e75c0379069d
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-09-25 16:26:32 +0300

    no build step

--- app.js
+++ app.js
@@ -2,13 +2,6 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var engine = require('ejs-locals');
-var middleware = require('./source/middleware');
-var applyAuthentication = require('./source/utils/applyAuthentication');
-var applyErrorLogging = require('./source/utils/applyErrorLogging');
 var config = require('./config');
 var logger = require('./source/utils/logger');
 var bootstrap = require('./source/bootstrap');
@@ -20,62 +13,70 @@ process.on('uncaughtException', function (err) {
 	console.log({msg:'Uncaught exception', error:err, stack:err.stack});
 });
 
-var app = express();
+bootstrap.app(function (err) {
+	if (err) {
+		throw err;
+	}
 
-app.configure(function(){
-	app.set('port', process.env.PORT || 3001);
-	app.engine('ejs', engine);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.use(express.favicon());
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(middleware.noCache());
-	app.use(middleware.errors.logHttpErrors());
-	app.use(app.router);
-});
+	var express = require('express');
+	var http = require('http');
+	var path = require('path');
+	var engine = require('ejs-locals');
+	var middleware = require('./source/middleware');
+	var applyAuthentication = require('./source/utils/applyAuthentication');
+	var applyErrorLogging = require('./source/utils/applyErrorLogging');
 
-app.configure('development', function() {
-	app.use(express.logger('dev'));
-	app.use(express.errorHandler());
-	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(middleware.serveMaster.development());
-});
+	var app = express();
 
-app.configure('test', function() {
-	app.use(express.errorHandler());
-	app.use(express.static(path.join(__dirname, 'public')));
-	app.use(middleware.serveMaster.development());
-});
+	app.configure(function(){
+		app.set('port', process.env.PORT || 3001);
+		app.engine('ejs', engine);
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'ejs');
+		app.use(express.favicon());
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(express.cookieParser());
+		app.use(middleware.noCache());
+		app.use(middleware.errors.logHttpErrors());
+		app.use(app.router);
+	});
 
-app.configure('staging', function() {
-	app.use(express.logger('short'));
-	app.use(express.compress());
-	app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneMonth }));
-	app.use(middleware.serveMaster.production());
-});
+	app.configure('development', function() {
+		app.use(express.logger('dev'));
+		app.use(express.errorHandler());
+		app.use(express.static(path.join(__dirname, 'public')));
+		app.use(middleware.serveMaster.development());
+	});
 
-app.configure('production', function() {
-	app.use(express.logger('short'));
-	app.use(express.compress());
-	app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneMonth }));
-	app.use(middleware.serveMaster.production());
-});
+	app.configure('test', function() {
+		app.use(express.errorHandler());
+		app.use(express.static(path.join(__dirname, 'public')));
+		app.use(middleware.serveMaster.development());
+	});
 
-require('./source/api')(app);
-require('./source/router')(app);
+	app.configure('staging', function() {
+		app.use(express.logger('short'));
+		app.use(express.compress());
+		app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneMonth }));
+		app.use(middleware.serveMaster.production());
+	});
 
-applyAuthentication(app, ['/api']);
-applyErrorLogging(app);
+	app.configure('production', function() {
+		app.use(express.logger('short'));
+		app.use(express.compress());
+		app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneMonth }));
+		app.use(middleware.serveMaster.production());
+	});
 
-//bootstrap.app(startApplicationServer);
+	require('./source/api')(app);
+	require('./source/router')(app);
 
-startApplicationServer();
+	applyAuthentication(app, ['/api']);
+	applyErrorLogging(app);
 
-function startApplicationServer () {
 	http.createServer(app).listen(app.get('port'), function() {
 		var env = process.env.NODE_ENV || 'development';
 		logger.info('Likeastore app listening on port ' + app.get('port') + ' ' + env + ' mongo: ' + config.connection);
 	});
-}
+});
--- source/bootstrap.js
+++ source/bootstrap.js
@@ -2,18 +2,18 @@ var exec = require('child_process').exec;
 var logger = require('./utils/logger');
 
 function bootstrapApp(callback) {
-	var env = process.env.NODE_ENV || 'development';
-	if (env === 'development') {
-		return callback (null);
-	}
+	// var env = process.env.NODE_ENV || 'development';
+	// if (env === 'development') {
+	// 	return callback (null);
+	// }
 
-	logger.info('bootrapping application...');
+	logger.info('bootrapping application in: ' + process.cwd());
 	logger.info('running grunt build...');
 
-	exec('./node_modules/grunt-cli/bin/grunt build', function (err) {
+	exec('./node_modules/grunt-cli/bin/grunt build', function (err, stdout, stderr) {
 		if (err) {
-			logger.error({message: 'failed to run grunt build.', err: err});
-			process.exit(1);
+			logger.info('stderr: ' + stderr);
+			return callback(err);
 		}
 
 		logger.info('build js and css sources done.');
