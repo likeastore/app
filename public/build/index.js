0f4b65698aa05623de3a91b59c759f672942bd23
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-09-25 15:04:06 +0300

    a bit more funny message for errors

--- public/build/index.js
+++ public/build/index.js
@@ -1,4 +1,4 @@
 module.exports = {
 	js: '/build/main-ced55e0f6025f5b462e8fb306e31fd91.js',
-	css: '/build/main-09a1e7bf8767218716c85e7cdc6f2979.css'
+	css: '/build/main-aebc67e6c4746952d217f16aeab76f4b.css'
 };
