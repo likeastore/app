b924aea6ceac0799cced36dd00b78d23ce916a41
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-09-29 11:53:56 +0300

    small corrections

--- source/utils/analytics.js
+++ source/utils/analytics.js
@@ -1,6 +1,7 @@
 var analytics = require('analytics');
 var config = require('../../config');
 
-var app = 'likeastore-' + process.env.NODE_ENV;
+var env = process.env.NODE_ENV || 'development';
+var app = 'likeastore-' + env;
 
 module.exports = analytics(app, config.analytics.url);
