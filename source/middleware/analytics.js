721be295e6766b0aff2e284dec74cabc72f28037
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-09-29 12:17:00 +0300

    more fixes

--- source/middleware/analytics.js
+++ source/middleware/analytics.js
@@ -3,7 +3,7 @@ var logger = require('../utils/logger');
 
 function track(event, data) {
 	return function (req, res, next) {
-		if (data.param) {
+		if (data && data.param) {
 			data[data.param] = req.params[data.param];
 			delete data.param;
 		}
