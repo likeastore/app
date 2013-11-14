3117f760b590a6e93f8e2ff95d13a9854e03d3f5
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-11-14 23:05:02 +0200

    corrected

--- source/utils/analytics.js
+++ source/utils/analytics.js
@@ -7,8 +7,10 @@ var app = 'likeastore-' + env;
 var options = {
 	server: config.analytics.url,
 	// TODO: move to config
-	username: 'alexanderbeletsky',
-	token: 'ebf6140a6314d7508a02300961636e9feffa73da'
+	credentials: {
+		username: 'alexanderbeletsky',
+		token: 'ebf6140a6314d7508a02300961636e9feffa73da'
+	}
 };
 
 module.exports = analytics(app, options);
