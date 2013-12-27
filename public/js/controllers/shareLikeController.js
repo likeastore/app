cbd68190e6d9dbad78891f28fc91d2a3ba8b8564
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-12-27 20:56:00 +0200

    preselect url

--- public/js/controllers/shareLikeController.js
+++ public/js/controllers/shareLikeController.js
@@ -1,9 +1,12 @@
 define(function () {
 	'use strict';
 
-	function ShareLikeController ($scope, links, ngDialog) {
+	function ShareLikeController ($scope, links) {
 		$scope.link = links.share($scope.$parent.ngDialogData);
 
+		setTimeout(function () {
+			document.getElementById('likeUrl').select();
+		}, 100);
 	}
 
 	return ShareLikeController;
