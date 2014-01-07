b561d65aeed8afe95e2bd8924d1f4e10c85df443
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-01-07 20:23:30 +0200

    update shared like texts

--- public/js/controllers/shareLikeController.js
+++ public/js/controllers/shareLikeController.js
@@ -2,7 +2,9 @@ define(function () {
 	'use strict';
 
 	function ShareLikeController ($scope, links) {
-		$scope.link = links.share($scope.$parent.ngDialogData);
+		$scope.link = links.share($scope.$parent.ngDialogData._id);
+		$scope.title = $scope.$parent.ngDialogData.title || 'Like';
+		$scope.service = $scope.$parent.ngDialogData.type;
 
 		setTimeout(function () {
 			document.getElementById('likeUrl').select();
