5d174e79adbdcc8ee424c8acbd1c9a6f099afff0
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-01-08 18:35:38 +0200

    add notifier about error

--- public/js/directives/stickyAt.js
+++ public/js/directives/stickyAt.js
@@ -1,17 +1,25 @@
 define(function (require) {
 	'use strict';
 
-	function StickyAt ($window) {
+	function StickyAt ($window, $rootScope) {
 		return {
 			restrict: 'A',
 			link: function (scope, elem, attrs) {
+				var offset = scope.$eval(attrs.stickyAt);
+
+				$rootScope.$watch('user.warningHidden', function (value) {
+					if (value) {
+						offset = 50;
+					}
+				});
+
+				$window.onscroll = function () {
+					elem.toggleClass('sticky', scrollTop() > +offset);
+				};
+
 				function scrollTop () {
 					return ($window.pageYOffset !== undefined) ? $window.pageYOffset : (document.documentElement || document.body).scrollTop;
 				}
-
-				$window.onscroll = function () {
-					elem.toggleClass('sticky', scrollTop() > +attrs.stickyAt);
-				};
 			}
 		};
 	}
