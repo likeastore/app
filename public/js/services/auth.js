e3c7b1d40c939cc1ead7b96f0634fb5535262302
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-01-14 13:04:10 +0200

    add safety add-on

--- public/js/services/auth.js
+++ public/js/services/auth.js
@@ -1,16 +1,20 @@
 define(function (require) {
 	'use strict';
 
-	function Auth ($http, $window, $cookies, $cookieStore, storage, api) {
+	function Auth ($http, $window, $cookies, $cookieStore, api) {
 		return {
 			setAuthorizationHeaders: function () {
-				$http.defaults.headers.common['X-Access-Token'] = $cookies.token || storage.get('token');
+				$http.defaults.headers.common['X-Access-Token'] = $cookies.token;
 			},
 
 			logout: function () {
 				api.save({ resource: 'auth', target: 'logout' }, {}, function () {
-					storage.remove('token'); // keep it for safety
+					// keep it for safety for early users
+					if ($cookies.token) {
+						document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+					}
 					$http.defaults.headers.common['X-Access-Token'] = null;
+
 					$window.location = '/logout';
 				});
 			}
