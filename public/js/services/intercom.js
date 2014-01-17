e996b16b30fd5f6e3c25fa5ba820186580563332
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-01-17 23:27:53 +0200

    corrected

--- public/js/services/intercom.js
+++ public/js/services/intercom.js
@@ -9,7 +9,7 @@ define(function (require) {
 						window.Intercom('boot', {
 							app_id: '8aa471d88de92f2f1f1a2fc08438fc69f4d9146e',
 							email: user.email,
-							created_at: Math.round(new Date(user.registered).getTime()/1000);,
+							created_at: Math.round(new Date(user.registered).getTime()/1000),
 							widget: {activator: '#IntercomDefaultWidget'}
 						});
 					}
