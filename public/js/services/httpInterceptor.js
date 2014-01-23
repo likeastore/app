f43c9bce53950e421a99cc2efdc8c38a23aae844
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-01-23 12:47:21 +0200

    handle 404 by redirect to root

--- public/js/services/httpInterceptor.js
+++ public/js/services/httpInterceptor.js
@@ -3,8 +3,15 @@ define(function () {
 
 	// list of uri with special handling
 	var whiteListUrls = [
-		'/api/emails/share'
+		'/api/emails/share',
+		'/api/networks/dribbble'
 	];
+
+	function checkResponseUrl(responseUrl) {
+		return function (url) {
+			return responseUrl.substr(0, url.length) === url;
+		};
+	}
 
 	function HttpInterceptor ($q, $window, $location) {
 		return {
@@ -17,12 +24,16 @@ define(function () {
 			},
 
 			'responseError': function (response) {
-				if (whiteListUrls.indexOf(response.config.url) !== -1) {
+				if (whiteListUrls.some(checkResponseUrl(response.config.url))) {
 					return $q.reject(response);
 				}
 
 				if (response.status === 401) {
 					$window.location = '/logout';
+				}
+
+				if (response.status === 404) {
+					$location.url('/');
 				}
 
 				if (response.status === 500) {
