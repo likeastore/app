f2a2ddf28979ce54207d8524c90eaa5ba183b664
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-02-06 14:58:55 +0200

    add search people to suggest improvements

--- public/js/directives/lazyAutocomplete.js
+++ public/js/directives/lazyAutocomplete.js
@@ -1,15 +1,16 @@
 define(function (require) {
 	'use strict';
 
-	var tmpl = '\
-		<ul class="lazy-autocomplete" ng-if="results.length">\
-			<li ng-repeat="result in results" class="lazy-autocomplete-item" ng-class="{\'last\': $last}">\
-				<a href="/u/{{result.name}}" class="mask-link"></a>\
-				<img ng-src="{{result.avatar}}" class="avatar" avatar-load> <span class="name">{{result.displayName || result.name}}</span>\
-			</li>\
-		</ul>';
+	var $el = require('angular').element;
+	var tmpl = '<ul class="lazy-autocomplete" ng-if="results.length">\
+					<li ng-repeat="result in results" class="lazy-autocomplete-item" ng-class="{\'last\': $last}">\
+						<a href="/u/{{result.name}}" class="mask-link"></a>\
+						<img ng-src="{{result.avatar}}" class="avatar" avatar-load>\
+						<span class="name">{{result.displayName || result.name}}</span>\
+					</li>\
+				</ul>';
 
-	function LazyAutocomplete ($timeout, api) {
+	function LazyAutocomplete ($compile, $timeout, $document, api) {
 		return {
 			restrict: 'A',
 			controller: function ($scope) {
@@ -28,8 +29,34 @@ define(function (require) {
 					}
 				});
 			},
-			compile: function (elem) {
-				elem.after(tmpl);
+			compile: function (element) {
+				element.addClass('lazy-autocomplete-input');
+				element.after(tmpl);
+
+				return function (scope, elem, attrs) {
+					$document.bind('click', function (e) {
+						var isInput = $el(e.target).hasClass('lazy-autocomplete-input');
+						var isAutocomplete = $el(e.target).hasClass('lazy-autocomplete-item');
+
+						if (isInput || isAutocomplete) {
+							return;
+						}
+
+						closeAutocomplete();
+					});
+
+					$document.bind('keyup', function (e) {
+						if (e.keyCode === 27) {
+							closeAutocomplete();
+						}
+					});
+
+					function closeAutocomplete () {
+						scope.searchTag = null;
+						scope.results = [];
+						scope.$apply();
+					}
+				};
 			}
 		};
 	}
