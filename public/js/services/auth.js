55e2682556e9c093a7eeb6bdbd7b930dffa94173
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-03-14 13:21:29 +0200

    remove unnecerry auth code

--- public/js/services/auth.js
+++ public/js/services/auth.js
@@ -3,13 +3,8 @@ define(function (require) {
 
 	function Auth ($http, $window, $cookies, $cookieStore, api, Intercom) {
 		return {
-			setAuthorizationHeaders: function () {
-				$http.defaults.headers.common['X-Access-Token'] = $cookies.token;
-			},
-
 			logout: function () {
 				api.save({ resource: 'auth', target: 'logout' }, {}, function () {
-					$http.defaults.headers.common['X-Access-Token'] = null;
 					Intercom.shutdown();
 					$window.location = '/logout';
 				});
