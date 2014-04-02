099256930c8fbf5265b2b42ab9a04ded52d50c5e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-02 13:50:18 +0300

    added search endpoints for collections

--- source/api/search.js
+++ source/api/search.js
@@ -4,17 +4,33 @@ var middleware = require('../middleware');
 function searchService(app) {
 	app.get('/api/search',
 		middleware.analytics.track('search', {query: 'text'}),
-		searchItemsByText);
+		searchItems);
 
-	function searchItemsByText(req, res, next) {
+	app.get('/api/search/collection',
+		middleware.analytics.track('search collection', {query: 'text'}),
+		searchCollections);
+
+	function searchItems(req, res, next) {
 		var query = req.query.text;
 
-		search.fullTextItemSearch(req.user, query, function (err, items) {
+		search.items(req.user, query, function (err, items) {
 			if (err) {
-				return next({message: 'failed to search items for user by query', user: req.user, query: query, error: err, status: 500});
+				return next({message: 'failed to search items', user: req.user, query: query, err: err, status: 500});
 			}
 
 			res.json(items);
+		});
+	}
+
+	function searchCollections(req, res, next) {
+		var query = req.query.text;
+
+		search.collections(req.user, query, function (err, collections) {
+			if (err) {
+				return next({message: 'failed to search collections', user: req.user, query: query, err: err, status: 500});
+			}
+
+			res.json(collections);
 		});
 	}
 }
--- source/models/search.js
+++ source/models/search.js
@@ -23,6 +23,29 @@ function fullTextItemSearch (user, query, callback) {
 	});
 }
 
+function fullTextCollectionsSearch(user, query, callback) {
+	if (!query) {
+		return callback(null, { data: [], nextPage: false });
+	}
+
+	db.collections.runCommand('text', { search: query.toString() }, function (err, doc) {
+		if (err) {
+			return callback(err);
+		}
+
+		if (doc && doc.errmsg) {
+			return callback(doc.errmsg);
+		}
+
+		var items = doc.results.map(function (result) {
+			return result.obj;
+		});
+
+		callback(null, { data: items, nextPage: false });
+	});
+}
+
 module.exports = {
-	fullTextItemSearch: fullTextItemSearch
+	items: fullTextItemSearch,
+	collections: fullTextCollectionsSearch
 };
