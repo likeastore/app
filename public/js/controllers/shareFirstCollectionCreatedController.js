503ca21ab577e0d1086a79923e6c38262b1fa311
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-04-03 11:47:58 +0300

    update badges

--- public/js/controllers/shareFirstCollectionCreatedController.js
+++ public/js/controllers/shareFirstCollectionCreatedController.js
@@ -15,10 +15,10 @@ define(function () {
 			facebook.share({
 				name: 'I\'ve just created my first collection on Likeastore!',
 				link: 'https://likeastore.com',
-				picture: 'https://likeastore.com/img/feed-box.png',
+				picture: 'https://tour.likeastore.com/img/hobbyist-badge-congratulations.png',
 				description: 'Wanna get all your favorites from different services organized?\n' +
-					'Create collections of your favorited content with code libraries, Instagram photos, tweets, links, fonts, Dribbble shots and many more ' +
-					'on https://likeastore.com and earn the limited edition of "Hypebeast" badge! ;)'
+					'Create collections of your liked content with code libraries, Instagram photos, tweets, links, fonts, Dribbble shots and many more ' +
+					'on https://likeastore.com and earn the limited edition of "Hobbyist" badge! ;)'
 			});
 		};
 	}
