7fa92067a9e8daa78ab8da8d937be122e56dcaa6
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-04-04 16:11:04 +0300

    remove follow button from suggest

--- public/js/controllers/suggestPeopleController.js
+++ public/js/controllers/suggestPeopleController.js
@@ -12,17 +12,6 @@ define(function () {
 			$scope.people = people;
 			appLoader.ready();
 		});
-
-		$scope.followPerson = function (id, $event, index) {
-			if ($event.target.className.indexOf('disabled') !== -1) {
-				return;
-			}
-
-			$scope.people[index].followDisabled = true;
-			api.save({ resource: 'users', target: 'me', verb: 'follow', suffix: id }, {}, function () {
-				$scope.people.splice(index, 1);
-			});
-		};
 	}
 
 	return SuggestPeopleController;
