dfa1b4b527fd0dc0d040e15065e3dc0a76284a8b
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-18 14:23:14 +0300

    corrected

--- source/models/collections.js
+++ source/models/collections.js
@@ -381,7 +381,7 @@ function followedBy(user, name, callback) {
 }
 
 function popular(user, callback) {
-	db.collections.find({public: true, followers: {$exist: true}}).limit(30, function (err, collections) {
+	db.collections.find({public: true, followers: {$exists: true}}).limit(50, function (err, collections) {
 		if (err) {
 			return callback(err);
 		}
