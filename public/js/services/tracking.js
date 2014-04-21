951feff2817dbfd2bd29386aa7faf1c3769e4796
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-04-21 19:21:09 +0300

    widget removed

--- public/js/services/tracking.js
+++ public/js/services/tracking.js
@@ -9,8 +9,7 @@ define(function (require) {
 						Intercom.boot({
 							app_id: '8aa471d88de92f2f1f1a2fc08438fc69f4d9146e',
 							email: user.email,
-							created_at: Math.round(new Date(user.registered).getTime()/1000),
-							widget: {activator: '#IntercomDefaultWidget'}
+							created_at: Math.round(new Date(user.registered).getTime()/1000)
 						});
 
 						mixpanel.alias(user.email);
