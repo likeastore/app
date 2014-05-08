33c28b77b96e17f252221e0bc1e38d3e36a00d4e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-05-08 15:14:03 +0300

    to int

--- source/middleware/paging.js
+++ source/middleware/paging.js
@@ -4,8 +4,8 @@ function paging() {
 	return function (req, res, next) {
 		req.paging = req.paging || {};
 
-		req.paging.page = req.query.page;
-		req.paging.pageSize = req.query.pageSize || config.app.pageSize;
+		req.paging.page = +req.query.page;
+		req.paging.pageSize = +req.query.pageSize || config.app.pageSize;
 
 		if (req.paging.pageSize > 256) {
 			return next({message: 'page size > 256 is not allowed', pageSize: req.paging.pageSize, status: 412});
