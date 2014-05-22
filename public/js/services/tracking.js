ff8ebcea59210c4ce0eb6a009a934ae2fc3b7df1
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-05-22 16:16:56 +0300

    test remove intercom boot

--- public/js/services/tracking.js
+++ public/js/services/tracking.js
@@ -6,11 +6,7 @@ define(function (require) {
 			boot: function () {
 				$rootScope.$watch('user', function (user) {
 					if (user && window.appConfig.tracking) {
-						Intercom.boot({
-							app_id: '8aa471d88de92f2f1f1a2fc08438fc69f4d9146e',
-							email: user.email,
-							created_at: Math.round(new Date(user.registered).getTime()/1000)
-						});
+
 
 						mixpanel.alias(user.email);
 						mixpanel.people.set({
