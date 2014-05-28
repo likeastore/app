719fda8ba8eff113e6f499a799433820aefe5c31
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-05-28 20:57:34 +0300

    allow credentials

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ bootstrap.app(function (err) {
 
 	var cors = function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Credentials', 'true');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Access-Token, X-Revision, Content-Type');
 
