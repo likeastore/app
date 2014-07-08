43590e9d9d7ea5f8855ca631ebf94baebc5741e5
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-08 10:47:43 +0300

    disabled search due to ES server down

--- source/models/search.js
+++ source/models/search.js
@@ -10,81 +10,86 @@ function fullTextItemSearch (user, query, paging, callback) {
 		return callback(null, { data: [], nextPage: false });
 	}
 
-	var page = paging.page || 1;
 
-	elastic.search({
-		index: 'items',
-		from: (page - 1) * paging.pageSize,
-		size: paging.pageSize,
-		body: {
-			query: {
-				filtered: {
-					query: {
-						common: {
-							_all: {
-								query: text,
-								cutoff_frequency: 0.002,
-								minimum_should_match: 2,
-								low_freq_operator: 'and'
-							}
-						}
-					},
-					filter: {
-						term: {
-							user: user.email
-						}
-					}
-				}
-			},
-			highlight: {
-				fields: {
-					description: { },
-					title: { },
-					source: { }
-				}
-			}
-		}
-	}, function (err, resp) {
-		if (err) {
-			return callback(err);
-		}
+	return callback(null, { data: [], nextPage: false });
 
-		var items = resp.hits.hits.map(function (hit) {
-			return _.omit(_.extend(hit._source, transform(hit.highlight)), 'userData');
-		});
+	// Temp disabled due to ES issues...
 
-		items = items.map(function (item) {
-			return track ? trackUrl(item, user, text) : item;
-		});
+	// var page = paging.page || 1;
 
-		callback(null, {data: items, nextPage: items.length === paging.pageSize});
-	});
+	// elastic.search({
+	// 	index: 'items',
+	// 	from: (page - 1) * paging.pageSize,
+	// 	size: paging.pageSize,
+	// 	body: {
+	// 		query: {
+	// 			filtered: {
+	// 				query: {
+	// 					common: {
+	// 						_all: {
+	// 							query: text,
+	// 							cutoff_frequency: 0.002,
+	// 							minimum_should_match: 2,
+	// 							low_freq_operator: 'and'
+	// 						}
+	// 					}
+	// 				},
+	// 				filter: {
+	// 					term: {
+	// 						user: user.email
+	// 					}
+	// 				}
+	// 			}
+	// 		},
+	// 		highlight: {
+	// 			fields: {
+	// 				description: { },
+	// 				title: { },
+	// 				source: { }
+	// 			}
+	// 		}
+	// 	}
+	// }, function (err, resp) {
+	// 	if (err) {
+	// 		return callback(err);
+	// 	}
 
-	function transform(highlight) {
-		var transformed = {};
+	// 	var items = resp.hits.hits.map(function (hit) {
+	// 		return _.omit(_.extend(hit._source, transform(hit.highlight)), 'userData');
+	// 	});
 
-		if (highlight && highlight.description && highlight.description.length > 0) {
-			transformed.descriptionHtml = highlight.description[0];
-		}
+	// 	items = items.map(function (item) {
+	// 		return track ? trackUrl(item, user, text) : item;
+	// 	});
 
-		if (highlight && highlight.title && highlight.title.length > 0) {
-			transformed.titleHtml = highlight.title[0];
-		}
+	// 	callback(null, {data: items, nextPage: items.length === paging.pageSize});
+	// });
 
-		if (highlight && highlight.source && highlight.source.length > 0) {
-			transformed.sourceHtml = highlight.source[0];
-		}
+	// function transform(highlight) {
+	// 	var transformed = {};
 
-		return transformed;
-	}
+	// 	if (highlight && highlight.description && highlight.description.length > 0) {
+	// 		transformed.descriptionHtml = highlight.description[0];
+	// 	}
 
-	function trackUrl(item, user, text) {
-		var track = {user: user.email, id: item._id, url: item.source, query: text};
-		var payload = new Buffer(JSON.stringify(track)).toString('base64');
-		var url = config.tracker.url + '/api/track?d=' + payload;
+	// 	if (highlight && highlight.title && highlight.title.length > 0) {
+	// 		transformed.titleHtml = highlight.title[0];
+	// 	}
 
-		return _.extend(item, {trackUrl: url});
-	}
+	// 	if (highlight && highlight.source && highlight.source.length > 0) {
+	// 		transformed.sourceHtml = highlight.source[0];
+	// 	}
+
+	// 	return transformed;
+	// }
+
+	// function trackUrl(item, user, text) {
+	// 	var track = {user: user.email, id: item._id, url: item.source, query: text};
+	// 	var payload = new Buffer(JSON.stringify(track)).toString('base64');
+	// 	var url = config.tracker.url + '/api/track?d=' + payload;
+
+	// 	return _.extend(item, {trackUrl: url});
+	// }
 }
 
 module.exports = {
