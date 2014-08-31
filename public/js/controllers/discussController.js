bd9da9884799b81134fc13a873790a37b0c18ef8
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-08-31 21:53:12 +0300

    css for comments form

--- public/js/controllers/discussController.js
+++ public/js/controllers/discussController.js
@@ -1,7 +1,15 @@
 define(function () {
 	'use strict';
 
-	function DiscussController ($scope) {
+	function DiscussController ($rootScope, $scope) {
+		var comments = $scope.comments = [];
+
+		$scope.postComment = function () {
+			var comment = {message: this.message, user: $rootScope.user, date: moment().toDate()};
+			this.message = '';
+
+			comments.push(comment);
+		};
 	}
 
 	return DiscussController;
