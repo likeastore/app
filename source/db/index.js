4e5f1c9a2398e88f5084692df77be36a63a646d4
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-11-18 13:00:59 +0100

    one connection per app

--- source/db/index.js
+++ source/db/index.js
@@ -1,11 +1,14 @@
 var mongo = require('mongojs');
 
+var connection;
+
 module.exports = function (config) {
-	var connection = config.connection || 'mongodb://localhost:27017/likeastoredb';
-	var db = mongo.connect(connection, ['users', 'networks', 'items', 'collections', 'subscribers']);
-	if (!db) {
-		throw new Error('could not connect to ' + connection);
+	if (!connection) {
+		connection = mongo.connect(config.connection, ['users', 'networks', 'items', 'collections', 'subscribers']);
+		if (!connection) {
+			throw new Error('could not connect to ' + config.connection);
+		}
 	}
 
-	return db;
+	return connection;
 };
